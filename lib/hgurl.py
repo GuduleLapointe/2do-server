6a8b8fd3302c579071d1943b1468a24806b0d994
Koen Martens <gmc@sonologic.nl>
2015-10-11 00:15:56 +0200

    add hgurl normalizer

---
+++ lib/hgurl.py
@@ -0,0 +1,16 @@
+class HgUrl(object):
+
+    dictionary = {
+        'grid.3rdrockgrid.com:8002 Enerdhil (113, 116, 38)' : 'grid.3rdrockgrid.com:8002:Enerdhil',
+        'grid.3rdrockgrid.com:8002 Enerdhil Enerdhil (113, 116, 38)' : 'grid.3rdrockgrid.com:8002:Enerdhil',
+        'Seanchai Library ~ Grid.Kitely.Com:8002:Seanchai' : 'grid.kitely.com:8002:Seanchai',
+        'Seanchai Library - Spaceworld (Grid.Kitely.Com:8002/Spaceworld)' : 'grid.kitely.com:8002/Spaceworld',
+        'ROB\'s Rock Island(HG enabled) >grid.3rdrockgrid.com:8002:ROBs Rock Island' : 'grid.3rdrockgrid.com:8002:ROBs Rock Island',
+    }
+
+    @staticmethod
+    def normalize(url):
+        if url in HgUrl.dictionary.keys():
+            return HgUrl.dictionary[url]
+        return url
+        
