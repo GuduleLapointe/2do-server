088b36a62aaf47f36a6127d295901d87e8ad82ef
Koen Martens <gmc@sonologic.nl>
2015-10-11 20:40:13 +0200

    lfgrid fetcher added

---
+++ fetcher/lfgridfetcher.py
@@ -0,0 +1,23 @@
+import requests
+from lib.event import Event
+from fetcher.icalfetcher import IcalFetcher
+import icalendar
+from lib.category import Category
+from lib.webcache import WebCache
+
+class LfGridFetcher(IcalFetcher):
+    def __init__(self,webcache=None):
+        super(LfGridFetcher,self).__init__("http://www.localendar.com/public/lfgrid?style=X2", [Category("grid-littlefield")])
+        self.webcache = webcache
+
+if __name__=='__main__':
+    cache = WebCache("data/test_littlefield.cache")
+
+    f = LfGridFetcher(cache)
+
+    e = f.fetch()
+
+    cache.flush()
+
+    for ev in e:
+        print str(ev)
