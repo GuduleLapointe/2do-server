338496cfa754c02148816db4cc038493ab16acfc
Koen Martens <gmc@sonologic.nl>
2015-10-11 19:05:32 +0200

    export times as timestrings in json exporter

--- exporter/web.py
+++ exporter/web.py
@@ -9,8 +9,8 @@ class JsonExporter(Exporter):
         for event in self.events:
             jsonevent = {
                 'title' : event.title.encode('utf-8'),
-                'start' : event.start.astimezone(pytz.utc).strftime("%s"),
-                'end' : event.end.astimezone(pytz.utc).strftime("%s"),
+                'start' : event.start.astimezone(pytz.utc).strftime("%c %Z"),
+                'end' : event.end.astimezone(pytz.utc).strftime("%c %Z"),
                 'description' : event.description.encode('utf-8'),
                 'hgurl' : event.hgurl.encode('utf-8'),
                 'categories' : map(lambda e: str(e), event.categories),
