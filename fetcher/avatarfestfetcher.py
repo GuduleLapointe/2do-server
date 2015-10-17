5f1a07add324cd297529692335cc2e283863e573
Koen Martens <gmc@sonologic.nl>
2015-10-17 17:14:47 +0200

    add avatarfest category

--- fetcher/avatarfestfetcher.py
+++ fetcher/avatarfestfetcher.py
@@ -6,7 +6,7 @@ from lib.category import Category
 
 class AvatarFestFetcher(IcalFetcher):
     def __init__(self,webcache=None):
-        super(AvatarFestFetcher,self).__init__("https://www.google.com/calendar/ical/7qk2kkudtb4m864ljdmtsg308c%40group.calendar.google.com/public/basic.ics",[ Category("festival") ])
+        super(AvatarFestFetcher,self).__init__("https://www.google.com/calendar/ical/7qk2kkudtb4m864ljdmtsg308c%40group.calendar.google.com/public/basic.ics",[ Category("festival"), Category("grid-avatarfest") ])
         self.webcache = webcache
 
 if __name__=='__main__':
