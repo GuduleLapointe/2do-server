44901091eb3017ee7df5b30e543dcc46bc99a795
Koen Martens <gmc@sonologic.nl>
2015-11-07 19:36:25 +0100

    kitely: add regexps to kitely helper

--- helper/kitely.py
+++ helper/kitely.py
@@ -5,12 +5,9 @@ from lib.category import Category
 class KitelyHelper(Helper):
 
     dictionary = {
-        'Seanchai Library ~ Grid.Kitely.Com:8002:Seanchai' : 'grid.kitely.com:8002:Seanchai',
-        'Seanchai Library - Spaceworld (Grid.Kitely.Com:8002/Spaceworld)' : 'grid.kitely.com:8002:Spaceworld',
-        'One Day Ahead Of The Moon At The Seanchai Library' : 'grid.kitely.com:8002:Seanchai',
-        'In The Shadows At The Seanchai Library' : 'grid.kitely.com:8002:Seanchai',
-        '' : 'grid.kitely.com:8002:Kitely Welcome Center',
-        ' ' : 'grid.kitely.com:8002:Kitely Welcome Center',
+        re.compile('Seanchai Library - Spaceworld', re.I) : 'grid.kitely.com:8002:Spaceworld',
+        re.compile('Seanchai' , re.I): 'grid.kitely.com:8002:Seanchai',
+        re.compile('^\s*$', re.I) : 'grid.kitely.com:8002:Kitely Welcome Center',
     }
 
     def customizeEvent(self, event):
@@ -18,8 +15,10 @@ class KitelyHelper(Helper):
 
         if event.title=="Kitely Merchant Fair":
             event.hgurl = "grid.kitely.com:8002:Kitely Hypergrid Merchant Fair"
-        elif event.hgurl in KitelyHelper.dictionary.keys():
-            event.hgurl = KitelyHelper.dictionary[event.hgurl]
+        else:
+            for exp in KitelyHelper.dictionary.keys():
+                if exp.search(event.hgurl):
+                    event.hgurl = KitelyHelper.dictionary[exp]
 
         if event.hgurl == "grid.kitely.com:8002:Seanchai":
             event.addCategory(Category('literature'))
