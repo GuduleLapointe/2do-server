a4f31e6284561fd9fe00fc2867600485a510f752
Koen Martens <gmc@sonologic.nl>
2015-11-15 16:05:46 +0100

    francogrid: add default hgurl to welcome region

--- fetcher/francogridfetcher.py
+++ fetcher/francogridfetcher.py
@@ -3,17 +3,22 @@ from lib.event import Event
 from fetcher.icalfetcher import IcalFetcher
 import icalendar
 from lib.category import Category
-from helper.helper import Helper
+from helper.francogrid import FrancoGridHelper
+from lib.webcache import WebCache
 
 class FrancoGridFetcher(IcalFetcher):
     def __init__(self,webcache=None):
-        super(FrancoGridFetcher,self).__init__("http://francogrid.org/evenements/ical",[ Category("grid-francogrid") ],webcache, Helper())
+        super(FrancoGridFetcher,self).__init__("http://francogrid.org/evenements/ical",[ Category("grid-francogrid") ],webcache, FrancoGridHelper())
         self.webcache = webcache
 
 if __name__=='__main__':
-    f = FrancoGridFetcher()
+    cache = WebCache('data/test_francogridfetcher.pck')
+
+    f = FrancoGridFetcher(cache)
 
     e = f.fetch()
+
+    cache.flush()
 
     for ev in e:
         print str(ev)
---
+++ helper/francogrid.py
@@ -0,0 +1,34 @@
+import re
+import pickle
+from datetime import timedelta
+from helper import Helper
+
+class FrancoGridHelper(Helper):
+    hgre = re.compile("^[^:]+:[0-9]+:[^:]+$")
+
+    def __init__(self):
+        self.regions = pickle.loads(file('data/gcgregions.pck').read())
+
+    def findRegion(self, data):
+        if data=='-' or data==' ':
+            return 'francogrid.org:8002:'
+        return None
+
+    def customizeEvent(self, event):
+        event = super(FrancoGridHelper, self).customizeEvent(event)
+
+        hgurl = self.findRegion(event.hgurl)
+        if hgurl!=None:
+            event.hgurl = hgurl
+
+        return event
+
+
+if __name__=='__main__':
+    import requests
+    
+    page = requests.get("http://map.greatcanadiangrid.ca/")
+
+    if page.status_code==200:
+        matches = map(lambda m: m.group(1), re.finditer('new MapWindow\("Region Name: (.*?)<br', page.text))
+        file('data/gcgregions.pck','w+').write(pickle.dumps(matches))
