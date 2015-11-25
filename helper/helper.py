279354ca4f20cee65636d3250fd8bde10a6222ad
Koen Martens <gmc@sonologic.nl>
2015-11-25 19:54:16 +0100

    helper: add categories and expressions

--- helper/helper.py
+++ helper/helper.py
@@ -7,7 +7,6 @@ class Helper(object):
     expr = {
         'title' : {
             re.compile('(^DJ.*)|(.*\sDgJ\s)', flags=re.I)    : 'music',
-            re.compile('community.*(meeting|gathering)', flags=re.I): 'social',
             re.compile('\sfair(\s|$)', flags=re.I)          : 'fair',
             re.compile('.*office hours*', flags=re.I)          : 'social',
             re.compile('.*literary.*', flags=re.I)          : 'literature',
@@ -18,14 +17,20 @@ class Helper(object):
             re.compile('lesung', flags=re.I)          : 'literature',
         },
         'description' : {
+            re.compile('\[\[Category:Art\]\]', flags=re.I)  : 'art',
+            re.compile('\[\[Category:Education\]\]', flags=re.I)  : 'education',
+            re.compile('\[\[Category:Fair\]\]', flags=re.I)  : 'fair',
+            re.compile('\[\[Category:Lecture\]\]', flags=re.I)  : 'lecture',
+            re.compile('\[\[Category:Literature\]\]', flags=re.I)  : 'literature',
+            re.compile('\[\[Category:Music\]\]', flags=re.I)    : 'music',
+            re.compile('\[\[Category:Roleplay\]\]', flags=re.I)  : 'roleplay',
+            re.compile('\[\[Category:Social\]\]', flags=re.I)  : 'social',
             re.compile('(^DJ.*)|(.*\sDJs?\s)', flags=re.I)    : 'music',
             re.compile('(singing|singer|songs)', flags=re.I)    : 'music',
             re.compile('(dancing|dance)', flags=re.I)    : 'music',
             re.compile('(classic rock|jazz|blues|guitar|instrument)', flags=re.I)    : 'music',
-            re.compile('\[\[Category:Music\]\]', flags=re.I)    : 'music',
-            re.compile('\[\[Category:Lecture\]\]', flags=re.I)  : 'lecture',
-            re.compile('\[\[Category:Social\]\]', flags=re.I)  : 'social',
             re.compile('role.?play', flags=re.I)  : 'roleplay',
+            re.compile('para.?rp', flags=re.I)  : 'roleplay',
             re.compile('music', flags=re.I)  : 'music',
             re.compile('karaoke', flags=re.I)  : 'music',
             re.compile('\sooc\s', flags=re.I)  : 'roleplay',
@@ -33,8 +38,19 @@ class Helper(object):
             re.compile('\smeet\s', flags=re.I)  : 'social',
             re.compile('\sstory\s', flags=re.I)  : 'literature',
             re.compile('avatar repertory theat', flags=re.I)  : 'art',
-            re.compile('community', flags=re.I)  : 'social',
         },
+        'any' : {
+            re.compile('community.*(meeting|gathering)', flags=re.I): 'social',
+            re.compile('role.?play', flags=re.I)  : 'roleplay',
+            re.compile('para.?rp', flags=re.I)  : 'roleplay',
+            re.compile('community', flags=re.I)  : 'social',
+            re.compile('vernissage', flags=re.I)  : 'art',
+            re.compile('exhibition', flags=re.I)  : 'art',
+            re.compile('paintings', flags=re.I)  : 'art',
+            re.compile('paintings', flags=re.I)  : 'art',
+            re.compile('author', flags=re.I)  : 'literature',
+            re.compile('Seanchai', flags=re.I)  : 'literature',
+        }
     }
 
     catexpr = re.compile('\[\[Category:[^]]+\]\]', flags=re.I)
@@ -80,6 +96,12 @@ class Helper(object):
         for expr in Helper.expr['description'].keys():
             if expr.search(event.description)!=None:
                 event.addCategory(Category(Helper.expr['description'][expr]))
+        for expr in Helper.expr['any'].keys():
+            if expr.search(event.title)!=None:
+                event.addCategory(Category(Helper.expr['any'][expr]))
+            if expr.search(event.description)!=None:
+                event.addCategory(Category(Helper.expr['any'][expr]))
+
 
         event.description = Helper.catexpr.sub('', event.description)
 
