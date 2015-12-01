9daa50832246f7806a87bc7462955b2a29fa33e1
Koen Martens <gmc@sonologic.nl>
2015-12-01 12:07:28 +0100

    opensimworldhelper: fix Cajun Country (dreamland) region

--- helper/opensimworld.py
+++ helper/opensimworld.py
@@ -6,13 +6,18 @@ from helper import Helper
 class OpensimworldHelper(Helper):
     hgre = re.compile("^[^:]+:[0-9]+:[^:]+$")
 
+    hgexp = {
+        re.compile("marina bay", flags=re.I)        : "login.greatcanadiangrid.ca:8002:Marina Bay",
+        re.compile("Hot Rod 50s Diner", flags=re.I) : "login.greatcanadiangrid.ca:8002:Dreamland",
+        re.compile("Cajun Country", flags=re.I)     : "login.greatcanadiangrid.ca:8002:Dreamland",
+    }
+
     def findRegion(self, data):
         if data==None:
             return None
-        if re.search("marina bay", data, flags=re.I):
-            return "login.greatcanadiangrid.ca:8002:Marina Bay"
-        if re.search("Hot Rod 50s Diner", data, flags=re.I):
-            return "login.greatcanadiangrid.ca:8002:Dreamland"
+        for exp in OpensimworldHelper.hgexp:
+            if exp.search(data):
+                return OpensimworldHelper.hgexp[exp]
         return None
 
     def customizeEvent(self, event):
