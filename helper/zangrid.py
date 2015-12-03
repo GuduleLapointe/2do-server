c9421125bcc9a0a852f6707cbeeba071382115f0
Koen Martens <gmc@sonologic.nl>
2015-12-03 18:44:59 +0100

    add regions to zangrid whitelist, refactor check

--- helper/zangrid.py
+++ helper/zangrid.py
@@ -4,23 +4,19 @@ from helper import Helper
 class ZanGridHelper(Helper):
     hgexpr = [
         re.compile('hg.zangrid.ch:8002:Aachen', re.I),
+        re.compile('hg.zangrid.ch:8002:Nedra', re.I),
+        re.compile('hg.zangrid.ch:8002:Partyland', re.I),
     ]
 
     def customizeEvent(self, event):
         event = super(ZanGridHelper, self).customizeEvent(event)
 
         if event.hgurl!=None:
-            whitelisted = False
             for exp in ZanGridHelper.hgexpr:
                 if exp.search(event.hgurl):
-                    whitelisted=True
-                    break
-            if not whitelisted:
-                return None
-        else:
-            return None
+                    return event
 
-        return event
+        return None
 
 if __name__=='__main__':
     pass
