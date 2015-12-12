768c60e3d9cd2105f0a643549657a6c9df79c1e5
Koen Martens <gmc@sonologic.nl>
2015-12-12 12:45:19 +0100

    reduce cache expire due to short-term cancellations on this grid

--- fetcher/zangridfetcher.py
+++ fetcher/zangridfetcher.py
@@ -10,8 +10,8 @@ class ZanGridFetcher(IcalFetcher):
     def __init__(self,webcache=None):
         super(ZanGridFetcher,self).__init__("https://calendar.google.com/calendar/ical/zangrid2015%40gmail.com/public/basic.ics", [Category("grid-zangrid")], webcache, ZanGridHelper())
         self.webcache = webcache
-        self.minexpiry = 1000
-        self.maxexpirty = 1800
+        self.minexpiry = 600
+        self.maxexpirty = 1000
 
 if __name__=='__main__':
     cache = WebCache("data/test_zangrid.cache")
