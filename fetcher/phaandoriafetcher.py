364105ad51c8ae996b9b0340660710e1b990c73f
gmc <gmc@sonologic.nl>
2015-12-14 20:30:53 +0000

    phaandoriafetcher: decrease cache expiry

--- fetcher/phaandoriafetcher.py
+++ fetcher/phaandoriafetcher.py
@@ -10,8 +10,8 @@ class PhaandoriaFetcher(IcalFetcher):
     def __init__(self,webcache=None):
         super(PhaandoriaFetcher,self).__init__("https://calendar.google.com/calendar/ical/ath2lhne44mfcsmqp66p0iu2jo%40group.calendar.google.com/public/basic.ics", [Category("grid-phaandoria")], webcache, PhaandoriaHelper())
         self.webcache = webcache
-        self.minexpiry = 1000
-        self.maxexpirty = 1800
+        self.minexpiry = 600
+        self.maxexpirty = 1200
 
 if __name__=='__main__':
     cache = WebCache("data/test_phaandoria.cache")
