925843fbc52ae0ef02030c557bb43d3c96ee0e78
Koen Martens <gmc@sonologic.nl>
2015-12-30 18:06:38 +0100

    francogrid helper: remove gcg region copy-pasted

--- helper/francogrid.py
+++ helper/francogrid.py
@@ -6,9 +6,6 @@ from helper import Helper
 class FrancoGridHelper(Helper):
     hgre = re.compile("^[^:]+:[0-9]+:[^:]+$")
 
-    def __init__(self):
-        self.regions = pickle.loads(file('data/gcgregions.pck').read())
-
     def findRegion(self, data):
         if data=='None' or data=='-' or data==' ':
             return 'francogrid.org:8002:'
@@ -23,12 +20,3 @@ class FrancoGridHelper(Helper):
 
         return event
 
-
-if __name__=='__main__':
-    import requests
-    
-    page = requests.get("http://map.greatcanadiangrid.ca/")
-
-    if page.status_code==200:
-        matches = map(lambda m: m.group(1), re.finditer('new MapWindow\("Region Name: (.*?)<br', page.text))
-        file('data/gcgregions.pck','w+').write(pickle.dumps(matches))
