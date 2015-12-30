d8540cb7cf057908575995b280ebe2e4e916b95c
Koen Martens <gmc@sonologic.nl>
2015-12-31 00:22:53 +0100

    lfhelper: refactor for dict of hgexp, add festival of lights

--- helper/lf.py
+++ helper/lf.py
@@ -3,14 +3,29 @@ from helper import Helper
 from lib.category import Category
 
 class LfHelper(Helper):
-    bondagere = re.compile("bondage ranch",re.I)
+    hgexp = re.compile('lfgrid.com:8002:([^:]+)', flags=re.I)
+
+    expr = {
+        re.compile("bondage ranch",flags=re.I)      : 'lfgrid.com:8002:Bondage Ranch',
+        re.compile("luminaria",flags=re.I)          : 'lfgrid.com:8002:Luminaria',
+        re.compile("festival of lights",flags=re.I) : 'lfgrid.com:8002:Luminaria',
+    }
 
     def customizeEvent(self, event):
         event = super(LfHelper, self).customizeEvent(event)
 
         if event.hgurl!=None and event.hgurl.strip()!="":
-            if LfHelper.bondagere.search(event.title):
-                event.hgurl = "lfgrid.com:8002:Bondage Ranch"
+            if LfHelper.hgexp.search(event.hgurl)==None:
+                for exp in LfHelper.expr:
+                    if exp.search(event.hgurl):
+                        event.hgurl = LfHelper.expr[exp]
+                        break
+
+        if event.hgurl==None or LfHelper.hgexp.search(event.hgurl)==None:
+            for exp in LfHelper.expr:
+                if exp.search(event.title):
+                    event.hgurl = LfHelper.expr[exp]
+                    break
 
         return event
 
