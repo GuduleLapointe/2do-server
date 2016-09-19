c6d95a6d0a9131f35f4c18faa3d7cac3c127fb87
gmc <gmc@sonologic.nl>
2016-09-19 18:49:23 +0000

    Additional zangrid hg regions.

--- helper/zangrid.py
+++ helper/zangrid.py
@@ -6,9 +6,13 @@ class ZanGridHelper(Helper):
         re.compile('hg.zangrid.ch:8002:Aachen', flags=re.I)		: "hg.zangrid.ch:8002:Aachen",
         re.compile('hg.zangrid.ch:8002:Nedra', flags=re.I)		: "hg.zangrid.ch:8002:Nedra",
         re.compile('hg.zangrid.ch:8002:Partyland', flags=re.I)		: "hg.zangrid.ch:8002:Partyland",
+        re.compile('hg.zangrid.ch:8002:HyperZan', flags=re.I)		: "hg.zangrid.ch:8002:HyperZan",
+        re.compile('hg.zangrid.ch:8002:Greece', flags=re.I)		: "hg.zangrid.ch:8002:Greece",
         re.compile('login.zangrid.ch:8002:Aachen', flags=re.I)		: "hg.zangrid.ch:8002:Aachen",
         re.compile('login.zangrid.ch:8002:Nedra', flags=re.I)		: "hg.zangrid.ch:8002:Nedra",
         re.compile('login.zangrid.ch:8002:Partyland', flags=re.I)	: "hg.zangrid.ch:8002:Partyland",
+        re.compile('login.zangrid.ch:8002:HyperZan', flags=re.I)	: "hg.zangrid.ch:8002:HyperZan",
+        re.compile('login.zangrid.ch:8002:Greece', flags=re.I)		: "hg.zangrid.ch:8002:Greece",
     }
 
     def customizeEvent(self, event):
