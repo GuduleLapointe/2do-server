fe9985297264a4398396f1c4df27273c7457b2f0
Koen Martens <gmc@sonologic.nl>
2016-09-28 21:25:48 +0200

    Update avatarfest fetcher/helper

--- fetcher/avatarfestfetcher.py
+++ fetcher/avatarfestfetcher.py
@@ -8,7 +8,7 @@ from helper.avatarfest import AvatarFestHelper
 class AvatarFestFetcher(IcalFetcher):
     def __init__(self, eventlist, webcache=None):
         super(AvatarFestFetcher,self).__init__(
-            "https://www.google.com/calendar/ical/7qk2kkudtb4m864ljdmtsg308c%40group.calendar.google.com/public/basic.ics",
+            "https://calendar.google.com/calendar/ical/9g0idps421vmlfukadao93tiu8%40group.calendar.google.com/public/basic.ics",
             [ Category("fair"), Category("grid-avatarfest") ],
             eventlist,
             webcache,
--- helper/avatarfest.py
+++ helper/avatarfest.py
@@ -3,12 +3,13 @@ from helper import Helper
 
 class AvatarFestHelper(Helper):
     dictionary = {
-        'Digiworldz; Club La Ola' : 'login.digiworldz.com:8002:Sirens Grotto',
-        'Club La Ola\'S 2Nd Avatar Fest Tie-In' : 'login.digiworldz.com:8002:Sirens Grotto',
+        ' AvatarFEST' : 'avatarfest.net:6000',
     }
 
     def customizeEvent(self, event):
         event = super(AvatarFestHelper, self).customizeEvent(event)
+        if event.hgurl is None:
+            event.hgurl = "avatarfest.net:6000"
         if event.hgurl in AvatarFestHelper.dictionary.keys():
             event.hgurl = AvatarFestHelper.dictionary[event.hgurl]
         return event
