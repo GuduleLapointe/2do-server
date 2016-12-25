fb783ef9e3195cfb4e9c1a71be9f876477b04d39
gmc <gmc@sonologic.nl>
2016-12-25 21:54:56 +0000

    Ignore some non-events

--- helper/islandoasis.py
+++ helper/islandoasis.py
@@ -6,11 +6,16 @@ class IslandOasisHelper(Helper):
 
     titlere = re.compile('^[a-zA-Z]*\s*[0-9]+[apAP]m\s+(.*)$', flags=re.I)
 
+    badre = re.compile('Monarch.*s Winter Garden', flags=re.I)
+
     hgexpr = {
     }
 
     def customizeEvent(self, event):
         event = super(IslandOasisHelper, self).customizeEvent(event)
+
+        if IslandOasisHelper.badre.search(event.title):
+            return None
 
         match = IslandOasisHelper.titlere.search(event.title)
         if match is not None:
