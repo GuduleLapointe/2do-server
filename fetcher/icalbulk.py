4f82a2097359ad5e25ba7f09f1811976e3817a84
Olivier van Helden <olivier@van-helden.net>
2023-06-30 14:38:10 -0400

    attempts to fix some exclusions rules ignored

--- fetcher/icalbulk.py
+++ fetcher/icalbulk.py
@@ -3,19 +3,28 @@ from lib.event import Event
 from fetcher.icalfetcher import IcalFetcher
 import icalendar
 from lib.category import Category
-from helper.helper import Helper
 import re
+import codecs
+
+try:
+    from helper import Helper
+except ImportError:
+    try:
+        from helper.helper import Helper
+    except ImportError:
+        raise ImportError("Failed to import Helper from both 'helper' and 'helper.helper' modules")
 
 class IcalBulk(IcalFetcher):
     def __init__(self, eventlist, webcache=None):
         config = self.read_config()
+        exclusions = self.load_exclusions()
         for grid in config:
             fetcher = IcalFetcher(
                 grid['calendar_url'],
                 [Category(grid['grid_name'])],
                 eventlist,
                 webcache,
-                GenericHelper(grid['default_region'])
+                GenericHelper(grid['default_region'], exclusions.get(grid['grid_name'], set()))
             )
             self.webcache = webcache
             self.minexpiry = 1000
@@ -37,11 +46,31 @@ class IcalBulk(IcalFetcher):
                     config.append(grid)
         return config
 
+    def load_exclusions(self):
+        exclusion_file = 'exclusions.txt'
+        exclusions = {}
+        try:
+            with codecs.open(exclusion_file, 'r', encoding='utf-8') as file:
+                for line in file:
+                    line = line.strip()
+                    if line:
+                        grid_name, event_title = line.split(None, 1)  # Split by tab or space
+                        exclusions.setdefault(grid_name, set()).add(event_title)
+        except IOError:
+            pass  # If exclusion file is not found, no exclusions will be applied
+        return exclusions
+
+    def customizeEvent(self, event):
+        event = super(IcalBulk, self).customizeEvent(event)
+        return event
+
 class GenericHelper(Helper):
     hgre = re.compile("^[^:]+:[0-9]+:[^:]+$")
 
-    def __init__(self, default_region):
+    def __init__(self, default_region, exclusions=None):
+        super(GenericHelper, self).__init__()
         self.default_region = default_region
+        self.exclusions = exclusions or set()
 
     def findRegion(self, data):
         if data == None or data == '' or data == '-':
@@ -54,8 +83,16 @@ class GenericHelper(Helper):
     def customizeEvent(self, event):
         event = super(GenericHelper, self).customizeEvent(event)
 
+        if isinstance(event.title, unicode):
+            event_title = event.title
+        else:
+            event_title = event.title.decode('utf-8')
+
+        if event_title in self.exclusions:
+            return None
+
         hgurl = self.findRegion(event.hgurl)
-        if hgurl != None:
+        if hgurl is not None:
             hgurl = re.sub(r'^.*:\/\/', '', hgurl)
             event.hgurl = hgurl
 
